278c0bd9f452649569bd306634023e1eaad5f784
David Lev <davidlevman@gmail.com>
2019-10-02 00:57:31 +0300

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { BmiCalculatorService } from './bmi-calculator.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'bmi-calculator';
+  resultOfBMI: number;
+  areLegalInputs: boolean;
+
+  constructor(private BMICalc: BmiCalculatorService) { 
+    this.resultOfBMI = null;
+    this.areLegalInputs = true;
+  }
+
+  getBMI(e: Event, weight: number, height: number) {
+    this.resultOfBMI = null;
+    this.areLegalInputs = true;
+    e.preventDefault();
+
+    if (weight <= 0 || height <= 0) this.areLegalInputs = false;
+
+    if (weight >= 2 && height >= 0.5) this.resultOfBMI = this.BMICalc.getBMI(weight, height); 
+    else this.resultOfBMI = null;
+  }
 }
---
+++ src/app/bmi-calculator.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BmiCalculatorService {
+
+  constructor() { }
+
+  // weight (kg), height (meters)
+  getBMI(weight: number, height: number) {
+    let bmi = Math.ceil(weight/ Math.pow(height, 2));
+    //let bmi = weight/ Math.pow(height, 2);
+    return bmi;
+  }
+}
